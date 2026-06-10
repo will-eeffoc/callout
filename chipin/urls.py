bb7921fa1f753cf33a6cbadb78441a07518e7817
will-eeffoc <foxtongreatrex@gmail.com>
2026-06-10 13:29:56 +1000

    Added search feature

--- chipin/urls.py
+++ chipin/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path("", views.home, name="home"),
+   path("search/", views.search_games, name="search"),
    path("add-game/", views.add_game, name="add_game"),
 ]
--- chipin/views.py
+++ chipin/views.py
@@ -1,13 +1,23 @@
 ﻿from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.db.models import Q
 from .forms import GameForm
 from .models import Review
+from users.models import Game
 
 @login_required
 def home(request):
     recent_reviews = Review.objects.select_related('game', 'user').all()[:10]
     return render(request, 'chipin/home.html', {'recent_reviews': recent_reviews})
+
+@login_required
+def search_games(request):
+    query = request.GET.get('q', '')
+    results = []
+    if query:
+        results = Game.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+    return render(request, 'chipin/search_results.html', {'results': results, 'query': query})
 
 @login_required
 def add_game(request):
